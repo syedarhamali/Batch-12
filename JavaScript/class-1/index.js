e48ed1072ad177a4bae110f4e881fc04ed0519c8
Arham~ <arhamali63@gmail.com>
2024-08-20 16:11:55 +0500

    js-class-1/2

---
+++ JavaScript/class-1/index.js
@@ -0,0 +1,14 @@
+// console.log("this is my second class 1") //statement
+// console.log("this is my second class 2") //statement
+// console.log("this is my second class 3") //statement
+// console.log("this is my second class 4") //statement
+// console.log("this is my second class 5") //statement
+
+const number1 = 5;
+const number2 = 10;
+const result = number1 + number2;
+
+console.log(number1)
+console.log(number2)
+
+console.log(result)
---
+++ JavaScript/class-2/index.js
@@ -0,0 +1,34 @@
+// alert("Arham Ali") 
+
+var myName;
+myName = 'Arham Ali'
+
+
+
+myName = "Arham"
+console.log(myName) 
+
+myName = "new Value"
+console.log(myName)
+
+
+var result = number1 - myName
+
+var myNewVariable
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
