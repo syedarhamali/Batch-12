cde358e57eb7a5d73e4131ff9dd01314460be715
Arham~ <arhamali63@gmail.com>
2024-08-22 16:00:23 +0500

    js-class-3

---
+++ JavaScript/class-3/index.js
@@ -0,0 +1,37 @@
+// var numberOne = 11;
+// var myName = prompt("Enter your name");
+// var numberTwo = 3;
+
+// console.log(myName)
+// var result = numberOne % numberTwo
+
+// alert(result)
+
+// if (myName == "Arham") {
+//   alert("ye teacher hain");
+// } else {
+//   console.log("ye student hai");
+// }
+
+var numberOne = prompt("Enter your first Number"); // phla number agaya
+var numberTwo = prompt("Enter your first Number"); // dosra number agaya
+var operator = prompt("Enter your Operator"); // kiya krwana hai
+
+if(operator == "+"){
+    var result = numberOne + numberTwo
+    alert(result)
+}
+
+if(operator == "-"){
+    var result = numberOne - numberTwo
+    alert(result)
+}
+
+if(operator == "*"){
+    var result = numberOne * numberTwo
+    alert(result)
+}
+if(operator == "/"){
+    var result = numberOne / numberTwo
+    alert(result)
+}
