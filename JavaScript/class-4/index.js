d93d93fcfc23c2af18c018bc8ee491aee80cce39
Arham~ <arhamali63@gmail.com>
2024-08-24 16:04:19 +0500

    class-4

---
+++ JavaScript/class-4/index.js
@@ -0,0 +1,21 @@
+// var num = 1 // number
+// var myName = "Arham Ali" // string
+// var khaliValue = null // null
+// var bgheerDefineHuiWiValue = undefined // undefined
+// var obj = {     // object
+//     myName: 'Arham',
+// }
+// var booleanValue = true || false // boolean
+
+// var country;
+// console.log(country)
+
+// var myName = prompt("Enter your name?");
+
+// if (myName == "Arham") {
+//   alert("It's a teacher");
+// } else if (myName == "Asharib") {
+//   alert("It's a teacher");
+// } else {
+//   alert("It's a Student");
+// }
