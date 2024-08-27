ea92ab9542e564395173a5c7a166baa487152849
Arham~ <arhamali63@gmail.com>
2024-08-27 15:54:09 +0500

    class-5

---
+++ JavaScript/class-5/index.js
@@ -0,0 +1,39 @@
+
+// single equal to assign kr raha hoga value ko 
+// double equals to check kr raha hoga value ko bagheer type dekhy
+// triple equals to check kr raha hoga value ko type k sath.
+
+// if(2 === 2){
+//     alert("ye dono brabar hen")
+// }
+
+// document.write("2 x 1 = 2" + "<br />")
+// document.write("Hello World" + "<br />")
+// document.write("Hello World")
+
+// generate a table 
+var userValue = prompt("Enter your table number");
+               
+document.write(userValue + " x " + " 1 = " + userValue * 1 + "<br />" )
+document.write(userValue + " x " + " 2 = " + userValue * 2 + "<br />" )
+document.write(userValue + " x " + " 3 = " + userValue * 3 + "<br />" )
+document.write(userValue + " x " + " 4 = " + userValue * 4 + "<br />" )
+document.write(userValue + " x " + " 5 = " + userValue * 5 + "<br />" )
+document.write(userValue + " x " + " 6 = " + userValue * 6 + "<br />" )
+document.write(userValue + " x " + " 7 = " + userValue * 7 + "<br />" )
+document.write(userValue + " x " + " 8 = " + userValue * 8 + "<br />" )
+document.write(userValue + " x " + " 9 = " + userValue * 9 + "<br />" )
+document.write(userValue + " x " + " 10 = " + userValue * 10 + "<br />" )
+
+
+
+
+// chapter 1 Alert
+
+// Q1
+
+alert("Welcome to Programming World")
+
+// Q2
+
+
