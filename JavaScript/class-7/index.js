be642623af3e285d75f1eaaf805cba8d1b955607
Arham~ <arhamali63@gmail.com>
2024-08-31 16:12:25 +0500

    class-7

---
+++ JavaScript/class-7/index.js
@@ -0,0 +1,32 @@
+
+//Nested if
+// if (age === 18) {
+//     if(isPakistani === "Pakistani"){
+//         alert("app vote de skty ho")
+//     }
+// }
+
+
+// var num = 3;
+
+// num++ //  + 1
+
+// console.log(num)
+
+
+// var score = 0;
+// var answer = prompt("Who is the founder of Pakistan");
+
+// if(answer === "Quaid-e-Azam"){
+//   score = score + 10
+// //   score++
+// }
+
+// document.write("Your score is " + score)
+
+
+var num = 3;
+
+num++
+                // 3   +  5     + 5
+console.log(num)
