ce6e3630569dec2894b9f3471f80c3d9aaa75b8a
Arham~ <arhamali63@gmail.com>
2024-09-10 15:57:38 +0500

    class-8

---
+++ JavaScript/class-8/index.js
@@ -0,0 +1,28 @@
+// var arrays = []
+
+// var cityNo1 = "Karachi"
+// var cityNo2 = "Islamabad"
+// var cityNo3 = "Lahore"
+
+// var Pakistan = [ "Karachi" , "Islamabad" , "Lahore" ]
+
+
+
+// console.log(Pakistan[3])
+
+// Pakistan.push("Hyderabad" , "Peshawar")
+
+// console.log(Pakistan)
+// console.log(Pakistan[3])
+
+
+// console.log(cityNo1)
+// console.log(cityNo2)
+// console.log(cityNo3)
+
+// console.log(Pakistan[3])
+
+
+var cars = ["Alto" , "mehran" , "Carola"]
+cars[1]="Audi"
+console.log(cars)
