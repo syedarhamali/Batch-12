57b2f664b2fb8002afec658cd81b861b4ee1ce6c
Arham~ <arhamali63@gmail.com>
2024-09-12 16:04:20 +0500

    class 9

---
+++ JavaScript/class-9/index.js
@@ -0,0 +1,11 @@
+var cities = ["Karachi" , "Lahore" , "Islamabad" , "Hyderabad"]
+
+var added = cities.push("Faislabad")
+
+var result = cities.pop()
+
+console.log(added , "return value of push method") // 5
+console.log(result , "return value of pop method") // Faislabad
+
+
+console.log(cities)
